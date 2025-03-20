a5800f596bc21301cf41f5ca2c0327067478e337
Carolyn Cui <33041211+catfeeshing@users.noreply.github.com>
2025-03-19 21:46:19 -0700

    the background works yippee！！！！！！！

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   themeColor: [
     { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    // { media: "(prefers-color-scheme: dark)", color: "black" },
   ],
 };
 
@@ -37,11 +37,32 @@ export default function RootLayout({
       <head />
       <body
         className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
+          // "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen font-sans antialiased",
           fontSans.variable,
         )}
       >
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+
+          {/* background image */}
+
+          <div className="fixed inset-0 -z-10">
+      <div 
+        className="absolute inset-0 bg-[url('../images/real_desktop.PNG')] bg-cover bg-center bg-no-repeat"
+        style={{ 
+          // implement this later idk why it doesn't recognise when i sub for backgroundImage when in the code it looks like it makes sense wahhh
+          // backgroundImage: `url('../images/real_desktop.PNG')`, 
+          width: "auto",
+          height: "auto",
+          // width: '10000px',
+          // height: '10000px',
+          filter: 'blur(3px)',
+          opacity: 0.3,
+        }}
+      />
+      {/* <div className="absolute inset-0 bg-gray-900/80" />  */}
+    </div>
+
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl px-6 flex-grow">
